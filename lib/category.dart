2060512f108af1bc55bed216429a68d26960a400
kong <acornversion@gmail.com>
2021-09-22 15:26:03 +0700

    update :  GridView.count to Category Page

--- lib/category.dart
+++ lib/category.dart
@@ -6,290 +6,72 @@ class Category extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List itemslist = [
-      {'title': 'คำศัพท์'},
-      {'title': 'สำนวน'},
-      {'title': 'การออกเสียง'},
-      {'title': 'เกม'},
-      {'title': 'หัวข้อ'}
+    List itemslist = ['คำศัพท์', 'สำนวน', 'การออกเสียง', 'เกม', 'หัวข้อ'];
+    List imagelist = [
+      'dictionary.png',
+      'ufo.png',
+      'speaking-1.png',
+      'puzzle.png',
+      'logo.png'
     ];
     return SafeArea(
         child: Scaffold(
-            body: SingleChildScrollView(
-                child: Container(
-                    child: Column(
+            body: Container(
+                child: GridView.count(
+      padding: const EdgeInsets.only(right: 15, left: 15, top: 10),
+      mainAxisSpacing: 0,
+      crossAxisSpacing: 0,
+      crossAxisCount: 2,
       children: [
-        // for (var item in itemslist)
-        //   Column(
-        //     children: [
-        //       Text(item['title']),
-        //       Text(item['title']),
-        //     ],
-        //   )
-        Container(
-            padding: EdgeInsets.only(top: 15),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton(
-                      style: ButtonStyle(
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(11),
-                      ))),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => CategoryCard()));
-                      },
-                      child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.2),
-                                spreadRadius: 5,
-                                blurRadius: 7,
-                                offset:
-                                    Offset(0, 3), // changes position of shadow
-                              ),
-                            ],
-                          ),
-                          padding: EdgeInsets.only(top: 40),
-                          width: 150,
-                          height: 150,
-                          child: Column(
-                            children: [
-                              Container(
-                                child: Image.asset(
-                                  'assets/images/dictionary.png',
-                                  width: 50,
-                                  height: 50,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Text(
-                                'คำศัพท์',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
-                          )),
-                    ),
-                    TextButton(
-                      style: ButtonStyle(
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(11),
-                      ))),
-                      onPressed: () {},
-                      child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.2),
-                                spreadRadius: 5,
-                                blurRadius: 7,
-                                offset:
-                                    Offset(0, 3), // changes position of shadow
-                              ),
-                            ],
-                          ),
-                          padding: EdgeInsets.only(top: 40),
-                          width: 150,
-                          height: 150,
-                          child: Column(
-                            children: [
-                              Container(
-                                child: Image.asset(
-                                  'assets/images/ufo.png',
-                                  width: 50,
-                                  height: 50,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Text(
-                                'สำนวน',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
-                          )),
-                    ),
-                  ],
+        for (var i = 0; i < itemslist.length; i++)
+          Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
                 ),
               ],
-            )),
-        Container(
-          child: Column(
-            children: [
-              Row(
+            ),
+            child: TextButton(
+              style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(11),
+              ))),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => CategoryCard()));
+              },
+              child: Container(
+                  child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  TextButton(
-                    style: ButtonStyle(
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(11),
-                    ))),
-                    onPressed: () {},
-                    child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.2),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset:
-                                  Offset(0, 3), // changes position of shadow
-                            ),
-                          ],
-                        ),
-                        padding: EdgeInsets.only(top: 40),
-                        width: 150,
-                        height: 150,
-                        child: Column(
-                          children: [
-                            Container(
-                              child: Image.asset(
-                                'assets/images/speaking-1.png',
-                                width: 50,
-                                height: 50,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            Text(
-                              'การออกเสียง',
-                              style: TextStyle(
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        )),
+                  Container(
+                    child: Image.asset(
+                      'assets/images/' + imagelist[i],
+                      width: 50,
+                      height: 50,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                  TextButton(
-                    style: ButtonStyle(
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(11),
-                    ))),
-                    onPressed: () {},
-                    child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.2),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset:
-                                  Offset(0, 3), // changes position of shadow
-                            ),
-                          ],
-                        ),
-                        padding: EdgeInsets.only(top: 40),
-                        width: 150,
-                        height: 150,
-                        child: Column(
-                          children: [
-                            Container(
-                              child: Image.asset(
-                                'assets/images/puzzle.png',
-                                width: 50,
-                                height: 50,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            Text(
-                              'เกม',
-                              style: TextStyle(
-                                color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        )),
-                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20),
+                    child: Text(
+                      itemslist[i].toString(),
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                    ),
+                  )
                 ],
-              ),
-            ],
+              )),
+            ),
           ),
-        ),
-
-        Container(
-            child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(11),
-                  ))),
-                  onPressed: () {},
-                  child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.2),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      padding: EdgeInsets.only(top: 40),
-                      width: 150,
-                      height: 150,
-                      child: Column(
-                        children: [
-                          Container(
-                            child: Image.asset(
-                              'assets/images/logo.png',
-                              width: 50,
-                              height: 50,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          Text(
-                            'หัวข้อ',
-                            style: TextStyle(
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
-                      )),
-                ),
-                Container(
-                    margin: EdgeInsets.all(10),
-                    width: 150,
-                    height: 150,
-                    child: Column(
-                      children: [],
-                    )),
-              ],
-            )
-          ],
-        ))
       ],
-    )))));
+    ))));
   }
 }
